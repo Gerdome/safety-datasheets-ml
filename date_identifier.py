5c7c7445ead764c1fbdfd9b37681a39e46b7b345
Adrian Pucher <adrian.pu2203@gmail.com>
2019-06-02 14:13:41 +0200

    Added several date formats

--- date_identifier.py
+++ date_identifier.py
@@ -22,29 +22,35 @@ data = pd.read_csv(datapath + 'data_0_50.csv')
 #get list of all words
 words = list(data.loc[:122,'word'])
 
-#dates = list()
+dates = list()
 
 for s in words:
     s = str (s)
-    for fmt in ('%d.%m.%Y', '%d.%m.%y', '%d-%m-%Y', '%d-%m-%y', '%d/%m/%Y', '%d/%m/%y','%d. %b %y', '%d. %B %y'):
+    for fmt in (#all short/long  combinations with dot format
+                '%d.%m.%Y', '%d.%m.%y', '%w.%m.%Y', '%w.%m.%y', '%d.%-m.%Y', '%d.%-m.%y','%w.%-m.%Y','%w.%-m.%y', 
+                '%Y.%m.%d', '%y.%m.%d', '%Y.%m.%w', '%y.%m.%w', '%Y.%-m.%d', '%y.%-m.%d','%Y.%-m.%w','%y.%-m.%w',
+                #all short/long  combinations with hyphen format
+                '%d-%m-%Y', '%d-%m-%y', '%w-%m-%Y', '%w-%m-%y', '%d-%-m-%Y', '%d-%-m-%y','%w-%-m-%Y','%w-%-m-%y', 
+                '%Y-%m-%d', '%y-%m-%d', '%Y-%m-%w', '%y-%m-%w', '%Y-%-m-%d', '%y-%-m-%d','%Y-%-m-%w','%y-%-m-%w',
+                #all short/long  combinations with slash format
+                '%d/%m/%Y', '%d/%m/%y', '%w/%m/%Y', '%w/%m/%y', '%d/%-m/%Y', '%d/%-m/%y','%w/%-m/%Y','%w/%-m/%y',
+                '%Y/%m/%d', '%y/%m/%d', '%Y/%m/%w', '%y/%m/%w', '%Y/%-m/%d', '%y/%-m/%d','%Y/%-m/%w','%y/%-m/%w',
+                #all integer/text combinations 
+                '%d. %b %y', '%d %b %y', '%d. %B %y', '%d %B %y', '%w. %b %y', '%w %b %y', '%w. %B %y', '%w %B %y'):
         try:
             date = datetime.strptime(s, fmt).date()
-            continue
+            break
         except ValueError:
             date = 'None'
 
-    #dates.append(date)
-    print(date)
+    dates.append(date)
 
-'''
 isDateColumn = pd.DataFrame ({'IsDate' :dates})
-
 data ['IsDate'] = isDateColumn
-
-data.loc['IsDate']
-
 data.to_csv(datapath + 'data_dates_identified_0_50_.csv', index=False, encoding='utf-8-sig')
-'''
+
+
+
 
 
 '''
