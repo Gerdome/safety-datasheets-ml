5053a86319616fe1092ef652b26c5db37932e5e1
Gerdome <merz.gerrit@gmail.com>
2019-06-19 17:27:11 +0200

    improved chap identifier

--- chap_identifier.py
+++ chap_identifier.py
@@ -19,12 +19,19 @@ data = pd.read_csv(datapath + 'data_all_avg_ordered.csv', dtype=str, index_col =
 #Add new colum for status if word is part of company name
 data['chapter'] = np.nan
 
+chapters = ['1','2','3','4','5','6','7','8',
+            '1.','2.','3.','4.','5.','6.','7.','8.']
+
+chapters_start_words = [':', 'Bezeichnung','Mögliche','Zusammensetzung','Erste-Hilfe-Maßnahmen','Maßnahmen','Handhabung','Begrenzung',
+                        'BEZEICHNUNG','MÖGLICHE','ZUSAMMENSETZUNG','ERSTE-HILFE-MAßNAHMEN','MAßNAHMEN','HANDHABUNG','BEGRENZUNG']
+
+
 #Beginning numbers of subchapter as stop words
 stop_list = ['1.1', '2.1', '3.1','4.1','5.1','6.1', '7.1', '8.1']
 
 #Loop through all rows in data
-for row in data.loc[:, ['word']].itertuples(index=True):
-    if row.word == 'ABSCHNITT':
+for row in data.loc[:300000, ['word']].itertuples(index=True):
+    if (row.word == 'ABSCHNITT' and ('Bold' in data.loc[row.Index,'font_name'] or int(data.loc[row.Index,'font_size']) > 10)) or (row.word in chapters and 'Bold' in data.loc[row.Index,'font_name'] and data.loc[row.Index+1,'word'] in chapters_start_words):
             print (row)
             #first token of chapter header
             i = 0
@@ -53,6 +60,7 @@ for row in data.loc[:, ['word']].itertuples(index=True):
                              
             #Set indicator if word is part of chapter label
             data.loc[row.Index:row.Index+(i-1), 'chapter'] = 1
+
 
 data.to_csv('chapter_identified.csv', encoding='utf-8-sig')
 
