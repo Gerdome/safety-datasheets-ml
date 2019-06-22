cb51afc1eb7f9d411ac007271646c62f07ec7d5e
Gerdome <merz.gerrit@gmail.com>
2019-06-22 10:08:40 +0200

    combine all identifiers

--- labeling.py
+++ labeling.py
@@ -3,3 +3,16 @@ import numpy as np
 import os 
 import re
 
+labels = ['chap', #1
+        'subchapter',#2
+        'chemicals',#3
+        'company',#4
+        'date',#5
+        'directive',#6
+        'signal',#7
+        'usecase',#8
+        'version']#9
+
+for l in labels:
+    print(l)
+    os.system("c:/Users/merzg/FZI_Seminar/safetysheets/env/Scripts/python.exe c:/Users/merzg/FZI_Seminar/safetysheets/safety-datasheets-ml/" + str(l) + "_identifier.py")
