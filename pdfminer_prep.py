eeb03a4d47ef86dbc4d380f5dba0903d7e7ee06f
Adrian Pucher <adrian.pu2203@gmail.com>
2019-06-26 00:52:08 +0200

    added new special char expression

--- pdfminer_prep.py
+++ pdfminer_prep.py
@@ -284,6 +284,7 @@ df = df.sort_values(['doc','Page','Ycord_first','Xcord_first'],ascending=[True,T
 df = df[ df["word"] != ""]
 
 #identify special characters
-df['special_char'] = df['word'].apply(lambda x: 1 if x in ('.',',','(', ')', '–', '[', '·','{', '}', ']', ':', ';', "'", '"','?', '/', '*','!', '@', '#', '&', '"*"', '`', '~', '$', '^', '+', '=', '<', '>') else 0 )
+df['special_char'] = df['word'].apply(lambda x: 1 if (x in ('"*"', 'ꞏ') or (len(x) == 1 and x.isalnum () == False)) else 0 )
+#= df['word'].apply(lambda x: 1 if x in ('.',',','(', ')', '–', '[', '·','{', '}', ']', ':', ';', "'", '"','?', '/', '*','!', '@', '#', '&', '"*"', '`', '~', '$', '^', '+', '=', '<', '>', 'ꞏ') else 0 )
 
 df.to_csv('data_all_ordered.csv', index=False, encoding='utf-8-sig')
